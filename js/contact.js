8cc38bdc48784ce14659e7e6a30713eee5b45e9f
RobbyMandala <robbymandala1@gmail.com>
2022-11-17 13:05:46 +0700

    oi

--- js/contact.js
+++ js/contact.js
@@ -1,14 +1,14 @@
-const btnReturn = document.querySelector(".btn-return");
-btnReturn.addEventListener("click", () => {
-  document.location.href = "../index.html";
-});
+// const btnReturn = document.querySelector(".btn-return");
+// btnReturn.addEventListener("click", () => {
+//   document.location.href = "../index.html";
+// });
 
-btnKirim.addEventListener("click", () => {
-  document.location.href = "../index.html";
-});
+// btnKirim.addEventListener("click", () => {
+//   document.location.href = "../index.html";
+// });
 
-const btnKirim = document.querySelector(".btn-kirim");
-btnKirim.addEventListener("click", () => {
-  document.location.href = "../index.html";
-  waktu();
-});
+// const btnKirim = document.querySelector(".btn-kirim");
+// btnKirim.addEventListener("click", () => {
+//   document.location.href = "../index.html";
+//   waktu();
+// });
